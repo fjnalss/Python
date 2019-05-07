c89e29b2983b0ddd3f469dc0a14ee8cf650d5bfd
lss <1552560896@qq.com>
2019-05-07 18:23:01 +0800

    20190507

--- learnpython/blogproject/blog/models.py
+++ learnpython/blogproject/blog/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+	"""文章的标签"""
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Category(models.Model):
+	"""文章的分类"""
+	name = models.CharField(max_length=100, default='其他')
+
+	def __str__(self):
+		return self.name
+
 
 class Article(models.Model):
 	title = models.CharField(max_length=100)
@@ -8,10 +23,15 @@ class Article(models.Model):
 	create_time = models.DateTimeField()
 	modified_time = models.DateTimeField()
 	page_views = models.IntegerField(default=0)
-	comment = models.TextField()
-	category = models.ForeiginKey(Category)
-	tags = models.ForeiginKey(Tag, blank=True)
-	author = models.ForeiginKey(User)
+	comment = models.TextField(blank=True)
+	category = models.ForeignKey(Category, default='其他', on_delete=models.SET_DEFAULT)
+	tags = models.ManyToManyField(Tag, blank=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
 
 
-class 
+
+
+		
--- learnpython/blogproject/blog/views.py
+++ learnpython/blogproject/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views import generic
+
+def IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
